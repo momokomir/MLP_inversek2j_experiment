e1b5e8bdb0b469f192e8ad7da517df454a945793
momokomir <jingooo@qq.com>
2016-10-12 21:55:09 +0800

    add test.py

---
+++ python_v/test.py
@@ -0,0 +1,23 @@
+# !/bin/usr/python
+
+###################################
+# main.py
+# qiao
+# 20161012 verion: v0.1
+#
+###################################
+
+from fann2 import libfann
+
+# test
+ann_test = libfann.neural_net()
+ann_test.create_from_file("inversek2j.net")
+testData = libfann.read_train_from_file("inversek2j_test.data")
+ann_test.test("testData")
+
+print "# Test MSE:%s" %(ann_test.get_MSE())
+
+ann_test.destroy()
+ann_train.destroy()
+
+
